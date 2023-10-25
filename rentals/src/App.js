426af5e2844b68c7427d5ab5ef93f748040b57f1
xkostolanskym <kostolansky.marek@gmail.com>
2023-10-25 14:29:09 +0200

    initial set up

---
+++ rentals/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
+
+function App() {
+  return (
+    <div className="bg-primary">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
