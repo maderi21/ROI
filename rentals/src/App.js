4a5ea5fdd1ac1dc4d858771e4e21e5f355099fcb
xkostolanskym <kostolansky.marek@gmail.com>
2023-10-26 21:28:24 +0200

    cols

--- rentals/src/App.js
+++ rentals/src/App.js
@@ -8,14 +8,28 @@ import Outcome from "./components/Outcome";
 
 function App() {
   return (
-    <div className="bg-primary">
-      <div className="container mt-5">
-        <Income />
-        <CashFlow />
-        <Expenses />
-        <CashRoi />
-        <Outcome />
-      </div>
+    <div className="bg-primary container">
+      <section>
+        <div className="row align-items-center">
+          <div className="col-md p-5">
+            <Income />
+          </div>
+          <div className="col-md p-5">
+            <CashFlow />
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="row align-items-center">
+          <div className="col-md p-5">
+            <Expenses />
+          </div>
+          <div className="col-md p-5">
+            <CashRoi />
+          </div>
+        </div>
+      </section>
+      <Outcome />
     </div>
   );
 }
