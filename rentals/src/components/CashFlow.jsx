745685fbda91f726d2bed4dde2768d2475fac630
xkostolanskym <kostolansky.marek@gmail.com>
2023-10-27 21:16:19 +0200

    flex on input

--- rentals/src/components/CashFlow.jsx
+++ rentals/src/components/CashFlow.jsx
@@ -6,16 +6,24 @@ const CashFlow = () => {
       <h2>CASH FLOW</h2>
       <div>
         <label htmlFor="monthlyRent">Rental Income</label>
-        <input type="number" id="monthlyRent" />
+        <input
+          type="number"
+          id="monthlyRent"
+          className="d-flex flex-row mb-3"
+        />
       </div>
       <div>
         <label htmlFor="monthlyRent">Rental Expenses</label>
-        <input type="number" id="monthlyRent" />
+        <input
+          type="number"
+          id="monthlyRent"
+          className="d-flex flex-row mb-3"
+        />
       </div>
       <div>
         <h3>TOTAL MONTHLY CASH FLOW</h3>
       </div>
-      <input type="number" id="monthlyRent" />
+      <input type="number" id="monthlyRent" className="d-flex flex-row mb-3" />
     </div>
   );
 };
--- rentals/src/components/Income.jsx
+++ rentals/src/components/Income.jsx
@@ -6,20 +6,32 @@ const Income = () => {
       <h2>INCOME</h2>
       <div>
         <label htmlFor="monthlyRent">Rental Income</label>
-        <input type="number" id="monthlyRent" />
+        <input
+          type="number"
+          id="monthlyRent"
+          className="d-flex flex-row mb-3"
+        />
       </div>
       <div>
         <label htmlFor="monthlyRent">Energy</label>
-        <input type="number" id="monthlyRent" />
+        <input
+          type="number"
+          id="monthlyRent"
+          className="d-flex flex-row mb-3"
+        />
       </div>
       <div>
         <label htmlFor="monthlyRent">Misc</label>
-        <input type="number" id="monthlyRent" />
+        <input
+          type="number"
+          id="monthlyRent"
+          className="d-flex flex-row mb-3"
+        />
       </div>
       <div>
         <h3>TOTAL MONTHLY INCOME</h3>
       </div>
-      <input type="number" id="monthlyRent" />
+      <input type="number" id="monthlyRent" className="d-flex flex-row mb-3" />
     </div>
   );
 };
