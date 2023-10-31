c76b56d7327cc643c49fea649da4671828334e17
xkostolanskym <kostolansky.marek@gmail.com>
2023-10-31 22:57:17 +0100

    cashFlow logic done

--- rentals/src/components/CashFlow.jsx
+++ rentals/src/components/CashFlow.jsx
@@ -1,44 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-const CashFlow = () => {
-  const [rentIncome, setRentIncome] = useState(0);
-  const [rentExpenses, setRentExpenses] = useState(0);
-
-  const handleRentIncome = (e) => {
-    setRentIncome(parseInt(e.target.value));
-  };
-
-  const handleExpenses = (e) => {
-    setRentExpenses(parseInt(e.target.value));
-  };
-
-  const cashFlow = rentIncome - rentExpenses;
-
+const CashFlow = ({ cashFlow, sumIncome, sumExpenses }) => {
   return (
     <div>
       <h2>CASH FLOW</h2>
-      <div>
-        <label htmlFor="monthlyRent">Rental Income</label>
-        <input
-          type="number"
-          value={rentIncome}
-          className="d-flex flex-row mb-3"
-          onChange={handleRentIncome}
-        />
-      </div>
-      <div>
-        <label htmlFor="monthlyRent">Rental Expenses</label>
-        <input
-          type="number"
-          value={rentExpenses}
-          className="d-flex flex-row mb-3"
-          onChange={handleExpenses}
-        />
-      </div>
+      <p>Rental Income: {sumIncome}</p>
+      <p>Rental Expenses: {sumExpenses}</p>
       <div>
         <h3>Total monthy cashflow is {cashFlow}</h3>
       </div>
-      <input type="number" id="monthlyRent" className="d-flex flex-row mb-3" />
     </div>
   );
 };
