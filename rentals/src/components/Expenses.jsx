a589180256613ec3f1888b37c4fadd031b81436c
xkostolanskym <kostolansky.marek@gmail.com>
2023-10-31 17:12:10 +0100

    Expenses dynamic count

--- rentals/src/components/Expenses.jsx
+++ rentals/src/components/Expenses.jsx
@@ -1,6 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Expenses = () => {
+  const [tax, setTax] = useState(0);
+  const [insurance, setInsurance] = useState(0);
+  const [utilities, setUtilities] = useState(0);
+  const [hoa, setHoa] = useState(0);
+  const [vacancy, setVacancy] = useState(0);
+  const [repairs, setRepairs] = useState(0);
+  const [capEx, setCapEx] = useState(0);
+  const [propertyMng, setPropertyMng] = useState(0);
+  const [mortgage, setMortgage] = useState(0);
+
+  const handleTax = (e) => {
+    setTax(parseInt(e.target.value));
+  };
+
+  const handleInsurance = (e) => {
+    setInsurance(parseInt(e.target.value));
+  };
+
+  const handleUtilities = (e) => {
+    setUtilities(parseInt(e.target.value));
+  };
+
+  const handleHoa = (e) => {
+    setHoa(parseInt(e.target.value));
+  };
+
+  const handleVacancy = (e) => {
+    setVacancy(parseInt(e.target.value));
+  };
+
+  const handleRepairs = (e) => {
+    setRepairs(parseInt(e.target.value));
+  };
+
+  const handleCapEx = (e) => {
+    setCapEx(parseInt(e.target.value));
+  };
+
+  const handlePropertyMng = (e) => {
+    setPropertyMng(parseInt(e.target.value));
+  };
+
+  const handleMortgage = (e) => {
+    setMortgage(parseInt(e.target.value));
+  };
+
+  const sumExpenses =
+    tax +
+    insurance +
+    utilities +
+    hoa +
+    vacancy +
+    repairs +
+    capEx +
+    propertyMng +
+    mortgage;
+
   return (
     <div>
       <h2>EXPENSES</h2>
@@ -8,63 +65,71 @@ const Expenses = () => {
         <label htmlFor="monthlyRent">Tax</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={tax}
           className="d-flex flex-row mb-2"
+          onChange={handleTax}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">Insurance</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={insurance}
           className="d-flex flex-row mb-2"
+          onChange={handleInsurance}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">Utilities</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={utilities}
           className="d-flex flex-row mb-2"
+          onChange={handleUtilities}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">HOA</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={hoa}
           className="d-flex flex-row mb-2"
+          onChange={handleHoa}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">Vacancy</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={vacancy}
           className="d-flex flex-row mb-2"
+          onChange={handleVacancy}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">Repairs</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={repairs}
           className="d-flex flex-row mb-2"
+          onChange={handleRepairs}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">CapEx</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={capEx}
           className="d-flex flex-row mb-2"
+          onChange={handleCapEx}
         />
       </div>
       <div>
         <label htmlFor="monthlyRent">Property Mng</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={propertyMng}
+          onChange={handlePropertyMng}
           className="d-flex flex-row mb-2"
         />
       </div>
@@ -72,14 +137,14 @@ const Expenses = () => {
         <label htmlFor="monthlyRent">Mortgage</label>
         <input
           type="number"
-          id="monthlyRent"
+          value={mortgage}
+          onChange={handleMortgage}
           className="d-flex flex-row mb-2"
         />
       </div>
       <div>
-        <h3>TOTAL MONTHLY EXPENSES</h3>
+        <h3>Total monthly expenses {sumExpenses}</h3>
       </div>
-      <input type="number" id="monthlyRent" className="d-flex flex-row mb-2" />
     </div>
   );
 };
